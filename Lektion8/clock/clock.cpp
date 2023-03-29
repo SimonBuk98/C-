341793620e37720ff9a373ab7ba0a5b7522804ad
Simon-Buk <simonbuk98@gmail.com>
2023-03-29 08:50:28 +0200

    Clock

--- Lektion8/clock/clock.cpp
+++ Lektion8/clock/clock.cpp
@@ -11,6 +11,7 @@ Clock::Clock(float dt, int h, int w):_dt(dt)
   for(int i =0; i <12; i++){
     cv::line(_canvas, cv::Point(cx+105*cos((M_PI/6)*i), cy+105*sin((M_PI/6)*i)), cv::Point(cx+125*cos((M_PI/6)*i), cy + 125*sin((M_PI/6)*i)), cv::Scalar(255,255,255));
   }
+  
 }
 
 void Clock::update(){
@@ -24,11 +25,12 @@ void Clock::draw(){
     _canvas.copyTo(img);
     int cx = _canvas.cols/2;
     int cy = _canvas.rows/2;
-
     
     // Draw clock arms
     double length = 100;
     cv::line(img, cv::Point(cx, cy), cv::Point(cx+length*cos(_angle), cy + length*sin(_angle)), cv::Scalar(255,255,255));
+    cv::circle(_canvas,cv::Point(cx+100*cos(_angle), cy + 100*sin(_angle)),3,cv::Scalar(0, 255, 255),-1);
+    
 
     // Draw clock center
     cv::circle(img, cv::Point(cx, cy), 3, cv::Scalar(0, 255, 0), -1);
